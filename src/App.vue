ceb047a556ef9089a5a872f336080d59b7ff5eda
Brooke Hedrick (he/him) <brooke.t.hedrick@gmail.com>
2021-04-17 15:31:04 +0000

    Name changes from helloworld to gdwcvuedemo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GdwcVueDemo msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GdwcVueDemo from './components/GdwcVueDemo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GdwcVueDemo
   }
 }
 </script>
